51d654b07e1ec8db76c8e4037bb065de819bbb38
Sanket Sonavane <2235668+sanketss84@users.noreply.github.com>
2019-12-29 07:33:54 +0530

    initial commit

---
+++ routes/subscribers.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const router = express.Router()
+const Subscriber = require('../models/subscriber')
+
+// Getting all
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+// Getting One
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber)
+})
+
+// Creating one
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel
+  })
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+// Updating One
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+// Deleting One
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove()
+    res.json({ message: 'Deleted Subscriber' })
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+//middlewares are awesome :)
+//middleware which is injected in various functions as this code was repeated in multiple places.
+//next is used to pass on the outcome to the next in-line code or middleware
+async function getSubscriber(req, res, next) {
+  let subscriber
+  try {
+    subscriber = await Subscriber.findById(req.params.id)
+    if (subscriber == null) {
+      return res.status(404).json({ message: 'Cannot find subscriber' })
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message })
+  }
+
+  res.subscriber = subscriber
+  next()
+}
+
+module.exports = router
